5bee7767a6b4eb1b4802d015303f690f9d6b4b30
haidarniv <haidarapps12@gmail.com>
2025-06-14 17:51:07 +0700

    progressing on hero section

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,13 +1,20 @@
+import { FaLinkedin } from "react-icons/fa";
+
 export default function HomePage() {
     return (
         <>
             <div className="bg-white">
-                <div className="container-main">
-                    <div className="flex flex-col">
+                <div className="container-main pt-20">
+                    <div className="flex flex-row justify-between">
+                        <div className="space-y-5">
+                            <h1 className="font-bold text-4xl">Turning Your Ideas into Real-World Digital Solutions</h1>
+                            <h3>I'm Haidar, a software engineer ready to help build your dream application.</h3>
+                            <button>View my projects</button>
+                            <FaLinkedin color="blue"></FaLinkedin>
+                        </div>
+
                         <div>
-                            <h1>Make your dreams come true with me</h1>
-                            <h3>I'm Haidar, a web developer</h3>
-                            <button>View my work</button>
+                            <img src="../src/assets/meReal.png" alt="photo Profile" className="w-[350px] h-auto mr-20"/>
                         </div>
 
                     </div>
