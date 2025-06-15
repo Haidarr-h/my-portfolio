6e8a23660cad38a6cc5a91218ee16ef8d8e79e79
haidarniv <haidarapps12@gmail.com>
2025-06-15 10:57:29 +0700

    done featured projects

--- src/data/Projects.js
+++ src/data/Projects.js
@@ -1,14 +1,20 @@
 export const projectList = [
     {
         title:"Evalyn: Automatic Grading with AI",
-        image_source: "../src/assets/evalyn.png",
+        image_source: "../src/assets/evalyn_dashboard.png",
         desc: "Modern form & grading website that uses AI to evaluate essays and give instant feedback.",
-        tags: ["React", "FastAPI", "Microsoft Azure OpenAI", "Postgre"],
+        tags: ["React", "FastAPI", "Microsoft Azure OpenAI", "PostgreSQL"],
     },
     {
-        title:"Upscale Website Cloning",
+        title:"Upscale",
         image_source: "../src/assets/upscale.png",
         desc: "Help businesses to get their perfect talents",
         tags: ["React", "Tailwindcss", "Framer Motion"],
+    },
+    {
+        title:"Automatic Google Maps Web Scrapping",
+        image_source: "../src/assets/web-scrap.png",
+        desc: "Scrapping 2.400.000 routes from Google Maps in 30 days with Python Automation",
+        tags: ["Python", "Selenium", "Docker", "Google Cloud"],
     },
 ]
--- src/pages/home/FeaturedProjects.jsx
+++ src/pages/home/FeaturedProjects.jsx
@@ -1,37 +1,34 @@
-import {projectList} from "../../data/Projects";
+import { projectList } from "../../data/Projects";
 
 export default function Projects() {
   return (
     <>
       <div className="bg-white">
-        <div className="container-main py-2">
-            <div>
-                <h1>Featured Projects</h1>
-                {/* container for projects */}
-                <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-                  {projectList.map((project, index) => (
-                    <a 
-                    key={index}
-                    href=""
-                    className="block rounded-xl bg-gray-300"
-                    >
-                      <img src={project.image_source} 
-                      alt={project.title} />
-                      <div>
-                        <h2>{project.title}</h2>
-                        <p>{project.desc}</p>
-                        {project.tags.map((tag, index) => (
-                          <span key={index}>{tag}</span>
-                        ))}
-                      </div>
-
-                    </a>
-
-                  ))}
-
-                </div>
-                
+        <div className="container-main py-5 lg:py-10">
+          <div className="space-y-5">
+            <h1 className="heading-section">Featured Projects</h1>
+            {/* container for projects */}
+            <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+              {projectList.map((project, index) => (
+                <a key={index} href="" className="block rounded-xl bg-[#F9FAFB]">
+                  <img
+                    src={project.image_source}
+                    alt={project.title}
+                    className="rounded-t-xl h-[200px] w-full"
+                  />
+                  <div className="flex flex-col gap-3 p-2 text-sm justify-between">
+                    <h2 className="font-bold text-base text-center text-highlight">{project.title}</h2>
+                    <p className="font-light">{project.desc}</p>
+                    <div className="space-x-1 mx-auto">
+                      {project.tags.map((tag, index) => (
+                        <p key={index} className="inline-block font-light bg-tags text-white">{tag}</p>
+                      ))}
+                    </div>
+                  </div>
+                </a>
+              ))}
             </div>
+          </div>
         </div>
       </div>
     </>
