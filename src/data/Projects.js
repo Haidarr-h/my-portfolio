ea61581fc343d6371e663fc4592675ba706669d6
haidarniv <haidarapps12@gmail.com>
2025-06-15 10:05:11 +0700

    add datas & minor changes about me

---
+++ src/data/Projects.js
@@ -0,0 +1,14 @@
+export const projectList = [
+    {
+        title:"Evalyn: Automatic Grading with AI",
+        image_source: "../src/assets/evalyn.png",
+        desc: "Modern form & grading website that uses AI to evaluate essays and give instant feedback.",
+        tags: ["React", "FastAPI", "Microsoft Azure OpenAI", "Postgre"],
+    },
+    {
+        title:"Upscale Website Cloning",
+        image_source: "../src/assets/upscale.png",
+        desc: "Help businesses to get their perfect talents",
+        tags: ["React", "Tailwindcss", "Framer Motion"],
+    },
+]
--- src/pages/home/FeaturedProjects.jsx
+++ src/pages/home/FeaturedProjects.jsx
@@ -1,3 +1,5 @@
+import {projectList} from "../../data/Projects";
+
 export default function Projects() {
   return (
     <>
@@ -5,6 +7,29 @@ export default function Projects() {
         <div className="container-main py-2">
             <div>
                 <h1>Featured Projects</h1>
+                {/* container for projects */}
+                <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+                  {projectList.map((project, index) => (
+                    <a 
+                    key={index}
+                    href=""
+                    className="block rounded-xl bg-gray-300"
+                    >
+                      <img src={project.image_source} 
+                      alt={project.title} />
+                      <div>
+                        <h2>{project.title}</h2>
+                        <p>{project.desc}</p>
+                        {project.tags.map((tag, index) => (
+                          <span key={index}>{tag}</span>
+                        ))}
+                      </div>
+
+                    </a>
+
+                  ))}
+
+                </div>
                 
             </div>
         </div>
